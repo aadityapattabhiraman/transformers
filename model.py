1ca0d05eea04db7003731a78e447b9138202c96c
aadityapattabhiraman <154038229+aadityapattabhiraman@users.noreply.github.com>
2024-02-21 14:22:51 +0530

    model.py

--- model.py
+++ model.py
@@ -56,3 +56,50 @@ class LayerNormailzation(nn.Module):
 		return self.alpha * (x - mean) / (std + self.eps) + self.bias
 
 
+class FeedForwardBlock(nn.Module):
+
+	def __init__(self, d_model: int, d_ff: int, dropout: float) -> None:
+		super().__init__()
+		self.linear_1 = nn.Linear(d_model, d_ff) # w1 and b1
+		self.dropout = nn.Dropout(dropout)
+		self.linear_2 = nn.Linear(d_ff, d_model) # w2 and b2
+
+	def forward(self, x):
+		# (batch, seq_len, d_model) --> (batch, seq_len, d_model) --> (batch, seq_len, d_model)
+		return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
+
+
+class MultiHeadAttentionBlock(nn.Module):
+
+	def __init__(self, d_model:int, h: int, dropout: float) -> None:
+		super.__init__()
+		self.d_model = d_model
+		self.h = h
+		assert d_model % h == 0, "d_model is not divisible by h"
+
+		self.d_k = d_model // h
+		self.w_q = nn.Linear(d_model, d_model) # Wq
+		self.w_k = nn.Linear(d_model, d_model) # Wk
+		self.w_v = nn.Linear(d_model, d_model) # Wv 
+
+		self.w_o = nn.Linear(d_model, d_model) # Wo 
+		self.dropout = nn.Dropout(dropout)
+
+	@staticmethod
+	def attention(query, key, value, mask, dropout: nn.Dropout):
+		d_k = query.shape[-1]
+
+		attention_scores = (query @ key.transpose(-2, -1)) / math.sqrt(d_k)
+		
+
+	def forward(self, q, k ,v, mask):
+		query = self.w_q(q) # (batch, seq_len, d_model) --> (batch, seq_len, d_model)
+		key = self.w_k(k) # (batch, seq_len, d_model) --> (batch, seq_len, d_model)
+		value = self.w_v(v) # (batch, deq_len, d_model) --> (batch, seq_len, d_model)
+
+		# (batch,seq_len, d_model) --> (batch, seq_len, h, d_k) --> (batch, h, seq_len, d_k)
+		query = query.view(query.shape[0], query.shape[1], self.h, self.d_k).transpose(1, 2)
+		key = key.view(key.shape[0], key.shape[1], self.h, self.d_k).transpose(1, 2)
+		value = key.view(key.shape[0], key.shape[1], self.h, self.d_k).transpose(1, 2)
+
+		x, self.attention_scores = MultiHeadAttentionBlock.attention(query, key, value, mask, self.dropout)
