778587c2aca7d6c0b4221dccde3a4b1e5d7f7b8a
aadityapattabhiraman <154038229+aadityapattabhiraman@users.noreply.github.com>
2024-02-21 10:26:01 +0530

    model.py

--- model.py
+++ model.py
@@ -14,3 +14,14 @@ class InputEmbeddings(nn.Module):
 	def forward(self, x):
 		return self.embedding(x) * math.sqrt(self.d_model)
 
+
+class PositionalEncoding(nn.Module):
+
+	def __init__(self, d_model: int, seq_len: int,dropout: float) -> None:
+		super().__init__()
+		self.d_model = d_model
+		self.seq_len = seq_len
+		self.dropout = nn.Dropout(dropout)
+
+
+		pe = torch.zeros(seq_len, d_model)
