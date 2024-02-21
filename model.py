76f7be8c454d7937d011315a6b68b514ef27c397
aadityapattabhiraman <154038229+aadityapattabhiraman@users.noreply.github.com>
2024-02-21 19:06:09 +0530

    model.py

--- model.py
+++ model.py
@@ -138,4 +138,19 @@ class EncoderBlock(nn.Module):
 		self.residual_connection = nn.ModuleList([ResidualConnection(dropout) for _ in range(2)])
 
 	def forward(self, x, src_mask):
-		x = 
+		x = self.residual_connection[0](x, lambda x: self.self_attention_block(x, x, x, src_mask))
+		x = self.residual_connection[1](x, self.feed_forward_block)
+		return x 
+
+
+class Encoder(nn.Module):
+
+	def __init__(self, layers: nn.ModuleList) -> None:
+		super().__init__()
+		self.layers = layers 
+		self.norm = LayerNormailzation()
+
+	def forward(self, x, mask):
+		for layer in self.layers:
+			x = layer(x, mask)
+		return self.norm(x)
