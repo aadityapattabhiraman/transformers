d4076500250b6dc6733ed99b7495dd76c4fee4d3
aadityapattabhiraman <154038229+aadityapattabhiraman@users.noreply.github.com>
2024-02-22 09:37:51 +0530

    model.py

--- model.py
+++ model.py
@@ -220,3 +220,7 @@ class Transformer(nn.Module):
 
 	def project(self, x):
 		return self.projection_layer(x)
+
+def build_transformer(src_vocab_size: int, tgt_vocab_size: int, src_seq_len: int, tgt_seq_len: int, d_model: int=512, N: int=6, h: int=8, dropout: float=0.1, d_ff=2048) -> Transformer:
+	# Create a embedding layer
+	src_embed = Input
